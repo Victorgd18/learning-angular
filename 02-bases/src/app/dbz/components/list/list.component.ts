4afc1aea8e4d99e3000c5e7a8ff0844c0a947955
Victorgdev <victorgdev@outlook.com>
2024-11-26 13:04:32 -0500

    Add DBZ module and main page component; update app structure and styles, before error compiler

---
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-list',
+  standalone: false,
+  template: `<p>list works!</p>`,
+  styleUrl: './list.component.css',
+})
+export class ListComponent { }
