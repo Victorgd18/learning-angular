67b41b8c5b511669482e9e35738b136decf79f5e
Victorgdev <victorgdev@outlook.com>
2024-11-27 10:16:26 -0500

    Implement character addition and listing functionality; update templates and create Character interface

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }];
+  
+
+  onDeleteCharacter(index: number): void {
+    console.log('Deleting character:', index);
+
+  }
 
 }
