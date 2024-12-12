39c8099ffa5c08d487d80624dfabe12d27a030eb
Victorgdev <victorgdev@outlook.com>
2024-12-12 09:11:48 -0500

    Implement lazy loading for GIF images and enhance card component display

---
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  standalone: false,
+
+  templateUrl: './lazy-image.component.html'
+})
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('url is required');
+  }
+
+  onLoad() {
+    console.log('loaded');
+    this.hasLoaded = true;
+  }
+
+}
