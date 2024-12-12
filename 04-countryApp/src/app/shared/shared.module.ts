d22c4274665a1e7950d2c7c023bb020c2d24ab86
Victorgdev <victorgdev@outlook.com>
2024-12-12 14:24:48 -0500

    Add countries module with routing and components for country-related pages

--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -5,6 +5,8 @@ import { RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
@@ -12,7 +14,9 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   declarations: [
     AboutPageComponent,
     HomePageComponent,
-    SidebarComponent
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule,
@@ -21,7 +25,9 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   exports: [
     AboutPageComponent,
     HomePageComponent,
-    SidebarComponent
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent
   ]
 })
 export class SharedModule { }
