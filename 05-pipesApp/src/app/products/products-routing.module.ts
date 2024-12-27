757368d79bfbe389ffd263fea66591f0fc787d0d
Victorgdev <victorgdev@outlook.com>
2024-12-27 14:38:16 -0500

    Update OrderComponent with custom pipes description and routing; modify menu item label and icon

--- 05-pipesApp/src/app/products/products-routing.module.ts
+++ 05-pipesApp/src/app/products/products-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { OrderComponent } from './pages/order/order.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 
 const routes: Routes = [
@@ -19,7 +21,7 @@ const routes: Routes = [
   },
   {
     path: 'custom',
-    component: UncommonPageComponent
+    component: OrderComponent
   },
   {
     path: '**',
