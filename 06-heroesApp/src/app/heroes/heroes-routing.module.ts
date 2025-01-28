f4b065b69b1852472ab60f694eb9b489cc48216d
Victorgdev <victorgdev@outlook.com>
2025-01-28 12:43:03 -0500

    Implement routing for authentication and heroes modules; add 404 error page component

--- 06-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ 06-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'heroes',
+    component: LayoutPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
